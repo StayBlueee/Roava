8f80807c5797028795513437a23b57dd711ec0c1
capyrightdev <capyrightdev@gmail.com>
2022-03-29 17:38:40 -0500

    Update FindByUserId.java

--- src/main/java/misc/FindByUserId.java
+++ src/main/java/misc/FindByUserId.java
@@ -4,7 +4,7 @@ import artifacts.user.User;
 
 public class FindByUserId {
     // Base, extremely simple for test.
-    public User getUserById() {
+    public User getUserById(String userId) {
         User user = null;
 
         return user;
