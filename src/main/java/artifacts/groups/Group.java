e9df11cd44690b5f961c0a6c29bcdc33e4d8e9d4
capyrightdev <capyrightdev@gmail.com>
2022-03-29 14:19:03 -0500

    Added more configuration
    Group

--- src/main/java/artifacts/groups/Group.java
+++ src/main/java/artifacts/groups/Group.java
@@ -4,9 +4,36 @@ import artifacts.user.User;
 
 import java.util.List;
 
-public interface Group {
+interface GroupInterface {
     String getName();
     String getId();
     Long getIdLong();
-    List<GroupRoles> getRole(User user);
+    List<GroupRoles> getRoles(User user);
+}
+
+public class Group implements GroupInterface {
+    String name;
+    String id;
+    Long idLong;
+    List<GroupRoles> roles;
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public Long getIdLong() {
+        return idLong;
+    }
+
+    @Override
+    public List<GroupRoles> getRoles(User user) {
+        return roles;
+    }
 }
