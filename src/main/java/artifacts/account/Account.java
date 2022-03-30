5018831c085ee2830a20d2143ebf8ef054ea39c5
StayBlueee <staybluesales@gmail.com>
2022-03-29 20:30:04 -0700

    Update Account.java

--- src/main/java/artifacts/account/Account.java
+++ src/main/java/artifacts/account/Account.java
@@ -3,22 +3,27 @@ package artifacts.account;
 import okhttp3.*;
 
 import java.io.IOException;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
 
 public class Account {
-    private OkHttpClient client = new OkHttpClient();
+    private OkHttpClient client;
 
     private MediaType JSON = MediaType.get("application/json; charset=utf-8");
 
-    public String xsrfToken;
+    private String robloSecurity;
+    private String xsrfToken;
 
-    public Response sendRequest(String url, RequestBody body) {
+    public String sendRequest(String url, RequestBody body) {
         Request request = new Request.Builder()
                 .url(url)
-                .addHeader("X-CSRF-TOKEN", xsrfToken)
+                .addHeader("X-CSRF-TOKEN", this.xsrfToken)
+                .addHeader("Content-Type", "application/json")
+                .addHeader("Cookie", ".ROBLOSECURITY=" + this.robloSecurity)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            return response;
+            return response.body().string();
         } catch (IOException e) {
             System.out.println("An error occurred while attempting to send your request!");
         }
@@ -27,16 +32,22 @@ public class Account {
     }
 
     public Account(String robloSecurity) {
+        this.client = new OkHttpClient().newBuilder()
+                .followRedirects(false)
+                .build();
+
         RequestBody body = RequestBody.create("", JSON);
 
         Request request = new Request.Builder()
                 .url("https://auth.roblox.com/v2/login")
-                .addHeader("Cookie", robloSecurity)
+                .addHeader("Cookie", ".ROBLOSECURITY=" + robloSecurity)
+                .addHeader("Content-Type", "application/json")
                 .post(body)
                 .build();
 
         try (Response response = client.newCall(request).execute()) {
-            this.xsrfToken = response.header("X-CSRF-TOKEN");
+            this.xsrfToken = response.header("x-csrf-token");
+            this.robloSecurity = robloSecurity;
         } catch (IOException e) {
             System.out.println("An error occurred while attempting to send your request!");
         }
