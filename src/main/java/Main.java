62bad7d7a81c2037afdcb299d28c098adda29747
StayBlueee <staybluesales@gmail.com>
2022-07-14 17:46:40 -0700

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,31 @@
 import artifacts.account.Account;
 import artifacts.exceptions.TokenNotValidException;
-import artifacts.exceptions.UserIdNotValid;
+import artifacts.groups.Group;
 import artifacts.user.User;
+import coresearch.cvurl.io.model.Response;
+import misc.Requester;
+
+import java.util.ArrayList;
 
 public class Main {
-    public static void main(String[] args) throws TokenNotValidException, UserIdNotValid {
-        Account account = new Account();
-        account.setAccount(null);
-        User user = new User("623426");
-        System.out.println(user.getUsername());
+    public static void main(String[] args) {
+        Account account;
+
+        try {
+            account = new Account("");
+        } catch (TokenNotValidException e) {
+            System.out.println(e.getMessage());
+            return;
+        }
+
+        User user = new User(1);
+
+        ArrayList<Group> groups = user.getGroups();
+
+        for (Group group : groups) {
+            System.out.println(group.getId() + " " + group.getName());
+        }
+
+        System.out.println("Logged in as " + account.getUserName() + " with user ID " + account.getUserId());
     }
 }
